f0cd9ffaf83adf7da9aab4abf2a3a8f9634ee704
Lilit0x <baasisek01@gmail.com>
2024-01-15 13:36:06 +0100

    chore: added plain text test

---
+++ tests/ingest.rs
@@ -0,0 +1,93 @@
+use std::{fs, path::Path, str};
+
+use anyhow::anyhow;
+use assert_cmd::Command;
+use rusqlite::Connection;
+
+#[derive(Debug, Clone)]
+struct UR {
+    _id: String,
+    _uri: String,
+    content: String,
+    nature: String,
+    size_bytes: u64,
+    // front_matter: Option<String>
+}
+
+fn ingest_fixtures() -> anyhow::Result<()> {
+    let mut fixtures_dir = std::env::current_dir()?;
+    fixtures_dir.push("support/test-fixtures");
+
+    let mut cmd = Command::cargo_bin("surveilr")?;
+    let output = cmd
+        .args([
+            "ingest",
+            "files",
+            "-d",
+            "e2e-test.db",
+            "-r",
+            fixtures_dir.to_str().unwrap(),
+        ])
+        .output()?;
+
+    if !output.status.success() {
+        eprintln!("Command failed with exit status: {}", output.status);
+        return Err(anyhow!("Command failed"));
+    }
+
+    Ok(())
+}
+
+fn execute_query(file_path: &Path) -> anyhow::Result<Vec<UR>> {
+    let mut db_path = std::env::current_dir()?;
+    db_path.push("e2e-test.db");
+    let conn = Connection::open(&db_path)?;
+    let mut stmt = conn.prepare(
+        "SELECT u.uniform_resource_id, u.uri, u.content, u.nature, u.size_bytes, u.f.ur_ingest_session_fs_path_entry_id
+FROM uniform_resource u
+JOIN ur_ingest_session_fs_path_entry f ON u.uniform_resource_id = f.uniform_resource_id
+WHERE u.uri = ?1;
+",
+    )?;
+
+    let iter = stmt.query_map([file_path.to_str().unwrap()], |row| {
+        Ok(UR {
+            _id: row.get(0)?,        // uniform_resource_id
+            _uri: row.get(1)?,       // uri
+            content: row.get(2)?,    // content
+            nature: row.get(3)?,     // nature
+            size_bytes: row.get(4)?, // size_bytes
+            // front_matter: row.get(5)?
+        })
+    })?;
+
+    fs::remove_file(&db_path)?;
+
+    let results: Result<Vec<_>, _> = iter.collect();
+    results.map_err(Into::into)
+}
+
+#[test]
+fn test_plain_text() -> anyhow::Result<()> {
+    ingest_fixtures()?;
+
+    let mut file_path = std::env::current_dir()?;
+    file_path.push("support/test-fixtures/plain-text.txt");
+
+    let rows = execute_query(&file_path)?;
+
+    assert_eq!(rows.len(), 1);
+    let resource = rows.get(0).unwrap();
+
+    let content = fs::read(&file_path)?;
+    let content = str::from_utf8(&content)?;
+    let metadata = fs::metadata(file_path)?;
+    let file_size = metadata.len();
+
+    assert_eq!(resource.content, content);
+    assert_eq!(resource.size_bytes, file_size);
+    assert_eq!(resource.nature, "txt");
+
+    Ok(())
+}
+
