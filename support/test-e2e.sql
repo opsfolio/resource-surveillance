3cf7ba694adb75a44911e8aa85e2c726a4d73587
Baasit <baasisek01@gmail.com>
2024-01-29 14:01:58 +0100

    docs(server): added SQL notebook for documentation

--- support/test-e2e.sql
+++ support/test-e2e.sql
@@ -1,15 +1,22 @@
 /*markdown
-Run osquery sample against active connection
+Start UDI-PGP with a config file. Find a sample config file in support/config-full.ncl.
+```bash
+surveilr udi pgp -c ./support/config.ncl
+```
+According to the configuration file PGP will start on the port 7777. Utilizing the VSCode SQL Editor to connect to UDI-PGP, connect to the 7777 port 
+with the database name being the name of the supplier, which in this case is "local-supplier".
+
+Run osquery sample against the active connection
 */
 
 select uuid, hostname from system_info;
 
 /*markdown
-Apply dynamic configuration to add supplier called "hetzner-atc"
+Add a new supplier called "hetzner-atc"
 */
 
 SET udi_pgp_serve_ncl_supplier = '
-  hetzner-atc = {
+  let hetzner-atc = {
     type = "osquery",
     mode = "local",
     atc-file-path = "./hetzner-atc.json",
@@ -19,4 +26,56 @@ SET udi_pgp_serve_ncl_supplier = '
         password = "supplier",
       },
     ],
-  }';
+  } in hetzner-atc';
+
+select id, person, code from party_role;
+
+/*markdown
+Add a remote supplier to the current configuration. To make this work, edit the name of the supplier(remote-supplier) and also any parameter you need to change.
+*/
+
+SET udi_pgp_serve_ncl_supplier = '
+  let remote-supplier = { 
+    type = "osquery",
+    mode = "remote",
+    ssh-targets = [
+      {
+        id = "one",
+        host = "157.245.40.97",
+        port = 22,
+        user = "oshuporu"
+      },
+      {
+        id = "me",
+        host = "localhost",
+        port = 222,
+        user = "lilit"
+      },
+      {
+        id = "do_test",
+        host = "128.199.1.17",
+        port = 22,
+        user = "root"
+      },
+    ],
+    auth = [
+      {
+        username = "baasit",
+        password = "supplier",
+      },
+    ],
+  } in remote-supplier';
+
+select uuid, hostname from system_info;
+
+/*markdown
+To edit the health and port addresses for UDI-PGP, you can use the following SET variable.
+*/
+
+SET udi_pgp_serve_ncl_core = '
+  let config = {  
+    addr = "127.0.0.1:5555",
+    metrics = 7777,
+    health = 9999 
+  } in config  
+';
