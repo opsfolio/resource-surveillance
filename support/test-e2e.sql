58cfaedaaf290af91b582dad3196399adfdd8889
Baasit <baasisek01@gmail.com>
2024-01-30 20:53:56 +0100

    docs(server): added documentation for SQL notebook

--- support/test-e2e.sql
+++ support/test-e2e.sql
@@ -1,21 +1,42 @@
 /*markdown
 Start UDI-PGP with a config file. Find a sample config file in support/config-full.ncl.
+
 ```bash
 surveilr udi pgp -c ./support/config.ncl
 ```
-According to the configuration file PGP will start on the port 7777. Utilizing the VSCode SQL Editor to connect to UDI-PGP, connect to the 7777 port 
-with the database name being the name of the supplier, which in this case is "local-supplier".
+According to the configuration file PGP will start on the port 7777. Utilizing the VSCode SQL Editor to connect to UDI-PGP, connect to the 7777 port. Since the intitial configuration is devoid of any supplier, we don't need a database name yet as is a username and and passowrd, they can all be left empty.
 
-Run osquery sample against the active connection
+Run osquery sample against the active connection to get the list of suppliers which should be empty.
 */
 
 SELECT * FROM udi_pgp_supplier;
 
 /*markdown
-All other 
+Query UDI-PGP for all other configuration parameters besides the suppliers, like the port and address PGP is bound to, the health and metrics addresses.
 */
 
 SEELCT * FROM udi_pgp_config;
+
+/*markdown
+To add a supplier to UDI-PGP, we can utilize `SET` queries with specific variable names. For example, to add a new local supplier to the existing configuration, execute the below cell
+*/
+
+SET udi_pgp_serve_ncl_supplier = '
+  let local-supplier = {
+    type = "osquery",
+    mode = "local",
+    atc-file-path = "./hetzner-atc.json",
+    auth = [
+      {
+        username = "john",
+        password = "pass",
+      },
+    ],
+  } in local-supplier';
+
+/*markdown
+Initiate a new connection to UDI-PGP through the SQL Notebook editor, set the database name to "local-supplier" and add the password as "pass" and username as "john" (the authentication details are described in the auth section of a supplier). Then, execute the below query against the new connection to UDI-PGP.
+*/
 
 select uuid, hostname from system_info;
 
@@ -30,16 +51,20 @@ SET udi_pgp_serve_ncl_supplier = '
     atc-file-path = "./hetzner-atc.json",
     auth = [
       {
-        username = "baasit",
-        password = "supplier",
+        username = "john",
+        password = "doe",
       },
     ],
   } in hetzner-atc';
 
+/*markdown
+Before executing the query, initiate another session with UDI-PGP through the SQL Notebook side panel. Use "hetzner" as the name of the database and fill in the username and password sections as described in the auth filed.
+*/
+
 select id, person, code from party_role;
 
 /*markdown
-Add a remote supplier to the current configuration. To make this work, edit the name of the supplier(remote-supplier) and also any parameter you need to change.
+Adding a remote supplier to the current configuration. To make this work, edit the name of the supplier(remote-supplier) and also any parameter you need to change.
 */
 
 SET udi_pgp_serve_ncl_supplier = '
@@ -68,22 +93,26 @@ SET udi_pgp_serve_ncl_supplier = '
     ],
     auth = [
       {
-        username = "baasit",
-        password = "supplier",
+        username = "john",
+        password = "doe",
       },
     ],
   } in remote-supplier';
 
+/*markdown
+Before executing the query, initiate another session with UDI-PGP through the SQL Notebook side panel. Use "remote-supplier" as the name of the database and fill in the username and password sections as described in the auth filed.
+*/
+
 select uuid, hostname from system_info;
 
 /*markdown
-To edit the health and port addresses for UDI-PGP, you can use the following SET variable.
+Core UDI-PGP configuration like the health address and the adress of the metrics port can be updated. The "addr" parameter is always ignored. 
 */
 
 SET udi_pgp_serve_ncl_core = '
   let config = {  
     addr = "127.0.0.1:5555",
-    metrics = 7777,
-    health = 9999 
+    metrics = "127.0.0.1:3333",
+    health = "127.0.0.1:5555" 
   } in config  
 ';
